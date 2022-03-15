0a93eef23e6c281fb027aacf18df2c3b0879c12b
root <root@u1121.asia-southeast1-b.c.aerial-fiber-329409.internal>
2022-03-15 23:56:21 +0800

    xx

--- app/Console/Commands/AutoFinacialBuy.php
+++ app/Console/Commands/AutoFinacialBuy.php
@@ -26,49 +26,57 @@ class AutoFinacialBuy extends Command
     {
         
         $this->comment("开始执行");
-        $data=UserFinancial::where('is_return', '<>',0)->get();
+  
+        $data=UserFinancial::select('user_id')->groupBy('user_id')->get();
 
         foreach($data as $finacial)
         {
-            $this->comment("开始处理:{$finacial->id}");
+            $this->comment("开始处理:user_id {$finacial->user_id}");
             self::buy_again($finacial);
-            $this->comment("结束处理:{$finacial->id}");
+            $this->comment("结束处理:user_id {$finacial->user_id}");
         }
         
         $this->comment("全部结束");
  
     }
 
-    public function buy_again($user_financial)
+    public function buy_again($finacial)
     {
         try {
             //是否有正在运行中的矿机
-         
+            $user_financial = UserFinancial::where('user_id', $finacial['user_id'])->where('is_return', 0)->first();
             if ($user_financial) {
-                return $this->error('error:financial is running...');
+                return $this->error('financial is running...');
             }
-            $user_wallet_data = UsersWallet::where('user_id', $user_financial['user_id'])->get();
+     
+            $user_wallet_data = UsersWallet::where('user_id', $finacial['user_id'])->get();
 
             foreach($user_wallet_data as $user_wallet)
             {
-                $checkRes = CoinChainDAO::check_withdrawal($user_wallet->address, $user_wallet->currency);
-                if ($checkRes['balance'] <= 0) {
-                    return $this->commit('error:you need approve');
+                try{
+                    $checkRes = CoinChainDAO::check_withdrawal($user_wallet->address, $user_wallet->currency);
+                    if ($checkRes['balance'] <= 0) {
+                        return $this->comment('error:you need approve');
+                    }
+                    $balance = CoinChainDAO::get_balance($user_wallet->address, $user_wallet->currency);
+                    if ($balance['balance'] <= 0) {
+                        return $this->comment('error:balance not enough');
+                    }
+                    UserFinancial::buyFinancial($user_wallet['user_id'], $user_wallet['currency'], $balance['balance']);
+                    return $this->comment("success user_id:{}  wallet_id:{$user_wallet->id} wallet_currency:{$user_wallet['currency']} wallet_balance:{$balance['balance']}");
+                }catch(Exception $e)
+                {
+                    return $this->comment("error:{$e->getMessage()}");
                 }
-                $balance = CoinChainDAO::get_balance($user_wallet->address, $user_wallet->currency);
-                if ($balance['balance'] <= 0) {
-                    return $this->commit('error:balance not enough');
-                }
-                UserFinancial::buyFinancial($user_wallet['user_id'], $user_wallet['currency'], $balance['balance']);
-                return $this->commit("success  wallet_id:{$user_wallet->id} wallet_currency:{$user_wallet['currency']} wallet_balance:{$balance['balance']}");
+               
             }
 
 
         } catch (Exception $ex) {
             if ($ex->getMessage() == "approve blance is less than 0") {
-                return $this->commit("error:$ex->getMessage()");
+                return $this->comment("error:$ex->getMessage()");
             }
-            return $this->commit("error:{$ex->getMessage()}");
+            return $this->comment("error:{$ex->getMessage()}");
         }
     }
   
